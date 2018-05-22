ed4cf20d196ffb52370a93ed3fb2855f69b62b6c
sudeep0909 <hst.dips@gmail.com>
2018-05-22 16:43:23 +0530

    Operators and expressions
    Write a JavaScript code to convert temperature from Celsius to Fahrenheit and vice-versa

---
+++ script.js
@@ -0,0 +1,20 @@
+
+// Javascript External File
+
+// variable temperatureInCelsius decleared and assign a value with 40
+let temperatureInCelsius = 40;
+
+// variable celsiusToFahrenheit and formula used to calculate the value with Fahrenheit scale
+var celsiusToFahrenheit = (temperatureInCelsius * 9)/5 + 32;
+
+// console both the values
+console.log("Temperature of " + temperatureInCelsius + " celsius, will be " + celsiusToFahrenheit + " fahrenheit");
+
+// variable temperatureInFahrenheit decleared and assign a value of Fahrenheit scale
+let temperatureInFahrenheit = celsiusToFahrenheit;
+
+//Calculate Fahrenheit To Celsius conversion and store result in variable
+let fahrenheitToCelsius = (temperatureInFahrenheit - 32) * 5 /9 ;
+
+// console both the values
+console.log("Temperature of " + temperatureInFahrenheit + " fahrenheit, will be " + fahrenheitToCelsius + " celsius");
